a46ff53ff29020a594abd316f4b730f8d2ee8279
Cassidy2024 <157302466+Cassidy2024@users.noreply.github.com>
2024-10-05 21:12:52 +0400

    created header

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header"> 
+        <img  className="header-image" src="./troll-face-removebg-preview.png" />
+        <h2 className="header-title" >Meme Generator</h2>
+        </header>
+    )
+}
